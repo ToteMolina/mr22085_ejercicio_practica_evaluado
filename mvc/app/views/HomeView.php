c128f662b0a1fcdc77210e0b8989930d25dddb58
ToteMolina <totelimo@gmail.com>
2025-09-25 12:29:51 -0600

    agregando pagina principal

--- mvc/app/views/HomeView.php
+++ mvc/app/views/HomeView.php
@@ -4,8 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Página principal</title>
+    <link rel="stylesheet" href="../public/css/style.css">
 </head>
 <body>
-    <h1>Hola mundoooooooo</h1>
+    <div class="container">
+        <div class="header">
+            <div class="inicio"><a href="">Inicio</a></div>
+            <div class="mi__lenguaje"><a href="">Mi lenguaje</a></div>
+            <div class="contactar"><a href="">Contactar</a></div>
+        </div>
+    </div>
 </body>
 </html>
